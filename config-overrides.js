bae114d3e16d4813c77f1bbf5ee5aff04f2a80d3
Chris Co <coswarm@gmail.com>
2020-01-10 18:58:07 +0300

    data layout config structure

---
+++ config-overrides.js
@@ -0,0 +1,41 @@
+/*
+ * customize-cra simply makes react-app-rewired config easier with helper functions
+ * (but it seems to ignore jest config set in package.json or jest.config.js, tested with "react-app-rewired": "^1.6.2")
+ */
+const { addBabelPlugins, addDecoratorsLegacy, override, useEslintRc, babelInclude } = require('customize-cra')
+const path = require('path')
+
+/* Override default CRA config */
+if (process.env.NODE_ENV === 'test') {
+  console.log(`⚡ config-override.js loaded in ${process.env.NODE_ENV} mode!`)
+  // Load different config to enable decorator in JEST test
+  module.exports = override(
+    useEslintRc(),  // to enable decorators before `export` keyword for intuitive developer experience
+    ...addBabelPlugins( // must have @babel/core@7.1.0
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          'legacy': true
+        }
+      ],
+      'transform-export-extensions'
+    )
+  )
+} else {
+  console.log('⚡ config-override.js loaded!')
+  module.exports = override(
+    babelInclude([
+      path.resolve('src'), // make sure you link your own source
+      path.resolve('../core/src'),
+    ]),
+    useEslintRc(),  // to enable decorators before `export` keyword for intuitive developer experience
+    addDecoratorsLegacy(),  // requires `@babel/plugin-proposal-decorators`
+    ...addBabelPlugins(
+      // todo: check if `lodash` plugin increases hot-reload time on large projects
+      'lodash',  // cherry pick imports to reduce final js bundle size, and increases build speed
+      'transform-export-extensions',
+      'import-graphql', // for importing .graphql and .gql files
+      // 'semantic-ui-react' imports are already pruned correctly by CRA 2
+    )
+  )
+}
---
+++ config.js
@@ -0,0 +1,43 @@
+const field = {
+  // Meta data
+  renderer: 'FieldsInGroup', // * name of React Component used to render this field
+  items: [], // recursively nested fields
+  hint: 'Displayed as title text above input',
+  readOnly: Boolean, // makes all nested fields disabled, with `readOnly` css class applied
+  style: Object, // css style to apply
+  className: 'CSS class name to apply',
+
+  // Generic input props
+  name: 'adminCosts.adminCategory', // * path to field value within *-data.json
+  label: 'Input label',
+  placeholder: 'To appear inside empty input when focused',
+  type: 'number', // 'text', 'textarea', 'email', etc.
+  unit: 'Input value unit label (e.x. "USD" for currency input)',
+  icon: 'dollar', // icon css class name -> displays Icon inside input
+  iconLeft: Boolean, // By default, icon is displayed to the right
+  onChange: Function, // callback for input value changes
+  float: Boolean, // whether label should float above input when in focus
+  disabled: Boolean, // disabled input
+  normalize: [Function], // redux-form input value normalizer function/s
+  validate: [Function], // redux-form input value validation function/s
+  value: undefined, // controlled input value
+  defaultValue: undefined, // to be used on init if `value` not set
+  info: 'Content to render when input is in focus',
+  error: 'Content to render when input is invalid',
+
+  // Dropdown input props (using react Semantic UI Dropdown)
+  compact: Boolean,
+  multiple: Boolean,
+  search: Boolean, // whether dropdown options are searchable
+  options: [{text: 'Label for Human', value: 'internal value'}],
+
+  // Slider input props
+  min: Number, // minimum value
+  max: Number, // maximum value
+  step: Number, // slider increment
+  pushable: Number, // minimum slider increments between two handles
+
+  // Upload and other input props
+  kind: 'Type of file or input, for example, "images"',
+  count: Number, // number of files/inputs
+}
