e01c2d5e45eb76d01ec0706a40c75336529f5f05
Chris Co <coswarm@gmail.com>
2020-02-13 16:09:11 +0300

    Placeholder component

--- repos/core/src/components/Placeholder.js
+++ repos/core/src/components/Placeholder.js
@@ -1,14 +1,40 @@
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
+import Emoji from './Emoji'
+import Text from './Text'
 import View from './View'
 
 /**
  * Placeholder - Pure Component.
  */
-export default function Placeholder (props) {
-  return <View fill className='full-screen middle center expand-v bg-texture' {...props}/>
+export default function Placeholder ({className, ...props}) {
+  return <View
+    fill
+    className={classNames('app__placeholder full-screen middle center fade-in-up padding-largest', className)}
+    {...props}
+  />
 }
 
 Placeholder.propTypes = {
   children: PropTypes.any.isRequired
+}
+
+Placeholder.Searching = function ({title, ...props}) {
+  return (
+    <Placeholder {...props}>
+      <Text className='h2 center'>{title}</Text>
+      <View className='signal-pulse large margin-v'>
+        <View className='position-center blink'><Emoji large>😎</Emoji></View>
+      </View>
+      <View className='margin-v'>
+        <Text className='center large margin-v-small'>{'Nothing found,'}</Text>
+        <Text className='center large margin-bottom'>{'check back in later...'}</Text>
+      </View>
+    </Placeholder>
+  )
+}
+
+Placeholder.Searching.propTypes = {
+  title: PropTypes.any.isRequired
 }
