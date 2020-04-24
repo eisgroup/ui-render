fc182832a2369e78b25a61d5f2cc3a82d2a07dda
Chris Co <coswarm@gmail.com>
2020-04-24 16:34:13 +0300

    List meta.json example

--- repos/core/src/pages/eis/data/list_meta.js
+++ repos/core/src/pages/eis/data/list_meta.js
@@ -1,10 +1,10 @@
 import meta from './expand-list_meta'
 
-delete meta.renderLabel
 export default {
-  ...meta,
-  view: 'RowList',
-  styles: 'justify wrap'
-  // row: true,
+  // view: 'List', // defaults to Column layout
+  // row: true, // using Row layout
+  view: 'RowList', // alternative way of defining Row layout
+  name: 'coverages',
+  styles: 'justify wrap', // should have `wrap` class added to collapse in mobile view
+  renderItem: meta.renderItem,
 }
-
