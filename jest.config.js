9992fc625318556bb27db3da4b8375483ab01bde
Chris Co <coswarm@gmail.com>
2020-07-01 11:30:02 +0300

    jest test setup

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testPathIgnorePatterns: ['node_modules/', '.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  },
+  // @Note: `ignore` patterns set inside babel.config.js will not be used by Jest
+  transformIgnorePatterns: [
+    'node_modules/(?!(dux-utils)/)',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+}
