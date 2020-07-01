c6ef66a13ab5d8d98b5f61ac4ef31938c17fae18
Chris Co <coswarm@gmail.com>
2020-07-01 11:14:07 +0300

    update packages

--- less-watcher-compiler.config.js
+++ less-watcher-compiler.config.js
@@ -5,27 +5,37 @@ module.exports = function () {
       {
         task: 'css', // name of the task to run, must be one of ['css', 'copy']
         watch: [ // file/s to watch for changes, using glob pattern
-          'style/**/*.less',
-          'style/override/**/*',
-          'style/theme.config'
+          'style/*.less', // only match files in root folder, sub-folders should have their own watcher
+          'style/fonts/**/*.less',
         ],
-        compile: 'style/all.less', // file/s to compile when changes detected, using glob pattern
+        compile: 'style/_all.less', // file/s to compile when changes detected, using glob pattern
         output: 'public/static/', // destination directory where new file/s should be saved
+        renameOptions: {basename: 'all'}, // change output file name
       },
       {
         task: 'copy', // copy over `compile` file/s to `output` directory without compilation
         watch: [
-          'style/fonts/**/*', // watch for changes of all file types within `style/fonts/` directory
+          'style/fonts/**/*.{eot,eof,svg,ttf,woff,woff2}', // watch for changes of all file types within `style/fonts/` directory
         ],
         compile: 'style/fonts/**/*.{eot,eof,svg,ttf,woff,woff2}', // only copy over files with matched extensions
         output: 'public/static/fonts/',
         renameOptions: {dirname: ''}, // make folder structure flat on output, using `gulp-rename` npm package
       },
+      /* Subtask to compile Semantic UI only to improve performance */
+      {
+        task: 'css',
+        watch: [
+          'style/override/**/*',
+        ],
+        compile: 'style/override/_semantic.less',
+        output: 'public/static/',
+        renameOptions: {basename: 'semantic'}, // change output file name
+      },
     ],
     symlinks: [ // useful for Semantic UI theme.config setup using `semantic-ui-less` library
       {
         // paths are relative to process.cwd()
-        target: 'style/theme.config', // path to the source file to reference
+        target: 'style/override/theme.config', // path to the source file to reference
         link: 'node_modules/semantic-ui-less/theme.config' // path to the file that references the target
       }
     ],
