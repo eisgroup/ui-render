fb15222eae185a7f26358ddf486f3b2066b1a26d
Chris Co <coswarm@gmail.com>
2020-07-02 21:53:13 +0300

    move to monorepo setup

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const withPlugins = require('next-compose-plugins')
-module.exports = withPlugins([
-  require('next-transpile-modules')([
-    // pass the modules you would like to see transpiled
-    'utils-pack'
-  ]),
-], {})
