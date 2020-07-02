657d9dd1d4e22524e96efef4fe86c9f3df1de0ce
Chris Co <coswarm@gmail.com>
2020-07-02 22:04:07 +0300

    initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * EXPORTS =====================================================================
+ * Modules' Exposing API - to enable consistent and maintainable app integration
+ * =============================================================================
+ */
+
+export * from './api'
+// export * from './form' // to be imported directly to reduce bundle size
