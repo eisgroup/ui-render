11bafea3bfef056fe599144f8f9ed50d3cdc990f
Chris Co <coswarm@gmail.com>
2020-07-31 12:12:26 +0300

    classic build without router and modal

--- repos/classic/src/main.js
+++ repos/classic/src/main.js
@@ -1,11 +1,9 @@
 // import Apollo from 'core/src/apollo'
-import App from 'core/src/App'
+import WebStudioPage from 'core/src/pages/eis/webstudio'
 import Redux from 'modules-pack/redux'
-import router from 'modules-pack/router'
-import { ROUTE, ROUTES } from 'modules-pack/variables'
 import React from 'react'
 import { render } from 'react-dom'
-import pages from './pages'
+import AppElement from './AppElement'
 import store from './store'
 
 /**
@@ -13,18 +11,11 @@ import store from './store'
  * =============================================================================
  */
 
-const {Router, history} = router
 render(
-  // <Apollo.Provider client={Apollo.client}>
   <Redux.Provider store={store}>
-    <Router history={history}>
-      <App>
-        {router.defineRoutes(ROUTES.FOR_DEFINITION, pages)}
-        {/*<Route exact path={ROUTE.SETTINGS} component={settings.View}/>*/}
-      </App>
-    </Router>
+    <AppElement>
+      <WebStudioPage/>
+    </AppElement>
   </Redux.Provider>
-  // </Apollo.Provider>
   , document.getElementById('react-app')
 )
-history.push(ROUTE.HOME)
