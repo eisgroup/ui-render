d208727fde15c4c6ffa7e4c1e1b6dfb072a8a892
Chris Co <coswarm@gmail.com>
2020-08-20 12:26:40 +0300

    install react-app-polyfill for classic

--- repos/classic/src/index.js
+++ repos/classic/src/index.js
@@ -1,7 +1,9 @@
+// This must be the first line in src/index.js
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 import 'core/src/common/variables'
 import './main'
 import * as serviceWorker from './serviceWorker'
-// import './zxcvbn'
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
