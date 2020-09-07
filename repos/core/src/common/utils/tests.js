df6a268c60356a018456714385321fae8c6d82db
Chris Co <coswarm@gmail.com>
2020-09-07 19:42:32 +0300

    Input tests

---
+++ repos/core/src/common/utils/tests.js
@@ -0,0 +1,6 @@
+export const withMarkup = (query) => (text) => query((content, node) => {
+  const hasText = (node) => node.textContent === text
+  const childrenDontHaveText = Array.from(node.children).every(child => !hasText(child))
+  return hasText(node) && childrenDontHaveText
+})
+
