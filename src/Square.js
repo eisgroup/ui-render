7bd5441610e889314ac507ca50f8724a9afa251d
Chris Co <coswarm@gmail.com>
2020-09-21 19:03:48 +0300

    refactor Square props

--- src/Square.js
+++ src/Square.js
@@ -11,20 +11,20 @@ import View from './View'
  * @Note: alternatively use @asSquare decorator, which works cross platform, but may not resize when screen shrinks
  * @Use: <Square.View> or <Square.Row>
  */
-function Square ({
+function SquareRender ({
   top, right, bottom, left,
   children, className, classNameInner, styleInner,
-  width = 1, height = 1, // ratio used to render rectangle (must be whole integer), default is square
+  wScale = 1, hScale = 1, // ratio used to render rectangle (must be whole integer), default is square
   ...p
 }) {
   const positioning = position({top, right, bottom, left})
   return (
     <View fill className={classNames('square', className, positioning)} {...p}>
       <View>
-        <canvas width={width} height={height}/>
+        <canvas width={wScale} height={hScale}/>
         <View className={classNames('square__inner position-fill', positioning)}>
           <View>
-            <canvas width={width} height={height}/>
+            <canvas width={wScale} height={hScale}/>
             <View className={classNames('square__inner position-fill', classNameInner)} style={styleInner}>
               {children}
             </View>
@@ -35,6 +35,7 @@ function Square ({
   )
 }
 
+const Square = React.memo(SquareRender)
 Square.propTypes = {
   center: PropTypes.bool,
   middle: PropTypes.bool,
@@ -49,8 +50,10 @@ Square.propTypes = {
 Square.View = asSquare(View)
 Square.Row = asSquare(Row)
 
+export default Square
+
 /**
- * React Component Decorator (HOC) to Render as Square, taking all available width and height
+ * React Component Decorator (HOC) to Render as Square/Rectangle, taking all available width and height
  * @example:
  *    @asSquare
  *    class UploadGrid extends Component {
@@ -60,8 +63,8 @@ Square.Row = asSquare(Row)
  * @param {Class|Function} Component - to render as square
  */
 export function asSquare (Component) {
-  return function Square ({top, right, bottom, left, width, height, className, ...props}) {
-    const ratio = width && height ? width / height : 1
+  return function Square ({top, right, bottom, left, wScale, hScale, className, ...props}) {
+    const ratio = wScale && hScale ? wScale / hScale : 1
     const isRectangle = ratio !== 1
     return <SizeMe monitorHeight children={({size: {width, height}}) => {
       const smallerSize = Math.floor(Math.min(width || 0, height || 0))
@@ -98,5 +101,3 @@ export function asSquare (Component) {
 function position ({top, right, bottom, left}) {
   return {middle: !bottom && !top, center: !left && !right, top, right, bottom, left}
 }
-
-export default React.memo(Square)
