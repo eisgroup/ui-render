4f962fbf05d34c301572d9e74c4a354baa22ff9a
Chris Co <coswarm@gmail.com>
2020-10-23 16:33:24 +0300

    classic widget intergration

---
+++ repos/core/src/pages/eis/classic/widget.js
@@ -0,0 +1,26 @@
+/**
+ * Request Data and Meta json for OpenL Widget
+ *
+ * @param {Object|Null|Undefined} dataJson - to update
+ * @param {Object|Null|Undefined} metaJson - to update
+ * @returns {Promise<{data: Object, meta: Object}>}
+ */
+window.POST = function (dataJson, metaJson) {
+  return new Promise(function (resolve, reject) {
+    window.POST.resolve = resolve
+    window.POST.reject = reject
+
+    if (dataJson == null) { // checks for undefined or null values
+      // initial load, no data provided
+      return onComplete(window.dataJson, window.metaJson)
+    } else {
+      // call to server to update dataJson (as object)
+      //...
+    }
+  })
+}
+
+function onComplete (dataJson, metaJson) {
+  if (!window.POST.resolve) return
+  window.POST.resolve({data: dataJson, meta: metaJson})
+}
