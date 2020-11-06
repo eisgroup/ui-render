5c02304c0cc0d1a1f891f6bf03ba99ae73bfba3e
Chris Co <coswarm@gmail.com>
2020-11-06 19:30:52 +0300

    refactor AutoSave to be customisable

--- src/form/views/AutoSave.js
+++ src/form/views/AutoSave.js
@@ -2,12 +2,12 @@ import React, { PureComponent } from 'react'
 import { FormSpy } from 'react-final-form'
 import { PropTypes } from 'react-ui-pack'
 import { Loading } from 'react-ui-pack/Loading'
-import { l, localiseTranslation, objChanges, TIME_DURATION_INSTANT } from 'utils-pack'
+import { debounce, l, localiseTranslation, objChanges, TIME_DURATION_INSTANT } from 'utils-pack'
 import { _ } from 'utils-pack/translations'
 
-if (!_.SAVING___) {
-  _.SAVING___ = {
-    [l.ENGLISH]: 'Saving...',
+if (!_.SYNCING___) {
+  _.SYNCING___ = {
+    [l.ENGLISH]: 'Syncing...',
   }
   localiseTranslation(_)
 }
@@ -15,73 +15,78 @@ if (!_.SAVING___) {
 /**
  * Final Form Auto Save on Input Value Changes
  * @example:
- *   <AutoSave save={console.warn} partial showLoader />
+ *   <AutoSave onChange={console.warn} partial showLoader />
  */
-class AutoSave extends PureComponent {
+export default class AutoSave extends PureComponent {
   static propTypes = {
-    // Async Function to call on input changes
-    save: PropTypes.func.isRequired,
+    // Async Function(values) to call on input changes
+    onChange: PropTypes.func.isRequired,
     // Whether to save only changed values, default is all Form values
     partial: PropTypes.bool,
     // Whether to overlay parent container with Loading spinner component
     showLoader: PropTypes.bool,
-    // Milliseconds to delay form save
-    debounce: PropTypes.number,
-    // Form values
-    values: PropTypes.object,
+    // FormSpy subscription
+    subscription: PropTypes.object,
+    // Milliseconds to delay form `onChange`
+    delay: PropTypes.number,
+    // Loading message
+    loadContent: PropTypes.any,
   }
 
   static defaultProps = {
-    debounce: TIME_DURATION_INSTANT,
+    delay: TIME_DURATION_INSTANT,
+    subscription: {values: true},
   }
 
   state = {
-    values: this.props.values,
+    values: undefined,
     submitting: false
   }
 
-  UNSAFE_componentWillReceiveProps (next, __) {
-    if (this.timeout) {
-      clearTimeout(this.timeout)
-    }
-    this.timeout = setTimeout(this.save, this.props.debounce)
+  UNSAFE_componentWillReceiveProps (next, _) {
+    if (next.delay !== this.props.delay) this.handleChange = debounce(this.onChange, next.delay)
   }
 
-  save = async () => {
-    if (this.promise) {
-      await this.promise
-    }
-    const {values, save, partial} = this.props
+  onChange = async ({values}) => {
+    if (this.state.values == null) return this.setState({values})
+
+    if (this.promise) await this.promise
+    const {onChange, partial} = this.props
 
     // This diff step is totally optional
     const changes = objChanges(this.state.values, values)
     if (changes) {
       // values have changed
       this.setState({submitting: true, values})
-      this.promise = save(partial ? changes : values)
+      this.promise = onChange(partial ? changes : values)
       await this.promise
       delete this.promise
       this.setState({submitting: false})
     }
   }
 
-  // This component doesn't have to render anything, but it can render submitting state.
-  render () {
-    const {showLoader} = this.props
+  handleChange = debounce(this.onChange, this.props.delay)
+
+  renderLoading = () => {
+    const {showLoader, loadContent} = this.props
     const {submitting} = this.state
     if (!showLoader || !submitting) return null
-    return <Loading isLoading>{_.SAVING___}</Loading>
+    return <Loading isLoading>{loadContent || _.SYNCING___}</Loading>
+  }
+
+  // Make a HOC
+  // This is not the only way to accomplish auto-save, but it does let us:
+  // - Use built-in React lifecycle methods to listen for changes
+  // - Maintain state of when we are submitting
+  // - Render a message when submitting
+  // - Pass in delay and save props nicely
+  // This component doesn't have to render anything, but it can render submitting state.
+  render () {
+    const {subscription} = this.props
+    return <>
+      {/* FormSpy onChange will be called once on component mount */}
+      <FormSpy subscription={subscription} onChange={this.handleChange}/>
+      {this.renderLoading()}
+    </>
   }
 }
-
-// Make a HOC
-// This is not the only way to accomplish auto-save, but it does let us:
-// - Use built-in React lifecycle methods to listen for changes
-// - Maintain state of when we are submitting
-// - Render a message when submitting
-// - Pass in debounce and save props nicely
-export default props => (
-  <FormSpy subscription={subscription} {...props} component={AutoSave}/>
-)
-
-const subscription = {values: true}
