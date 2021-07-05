23d70688c3f4a8a810527f56d2d01c2aecf51d53
Chris Co <coswarm@gmail.com>
2021-07-05 19:01:10 +0300

    localise validation messages

--- src/inputs/validationRules.js
+++ src/inputs/validationRules.js
@@ -1,15 +1,17 @@
-import { isEmpty, isNumeric, isPhoneNumber, pluralize, toList } from 'utils-pack'
+import { interpolateString, isEmpty, isNumeric, isPhoneNumber, pluralize, toList, toLowerCase } from 'utils-pack'
 import { _ } from 'utils-pack/translations'
 import { isGoodPassword } from 'utils-pack/utility'
 import isEmail from 'validator/lib/isEmail'
 import isLength from 'validator/lib/isLength'
 import isURL from 'validator/lib/isURL'
+import './translations'
 
 /**
  * VALIDATION RULES ============================================================
  * Common validation rules to be used with redux-form
  * =============================================================================
  */
+
 
 export const OK = undefined // Return type when validation passes
 
@@ -18,7 +20,7 @@ export function isRequired (value) {
 }
 
 export function url (value) {
-  return (value && !isURL(String(value), {require_protocol: true})) ? _.NOT_A_VALID_URL : OK
+  return (value && !isURL(String(value), {require_protocol: true})) ? _.INVALID_URL : OK
 }
 
 export function email (value) {
@@ -29,45 +31,45 @@ export function maxLength (length = 100) {
   return (value) => (
     isLength(String(value), {max: length})
       ? OK
-      : `Must be less than ${pluralize('character', length, true)}`
+      : interpolateString(_.MUST_BE_LESS_THEN_characters, {characters: pluralize(toLowerCase(_.CHARACTER), length, true)})
   )
 }
 
 export function password (value) {
   password.value = value
-  return (!value || isGoodPassword(value)) ? OK : 'Password is too weak'
+  return (!value || isGoodPassword(value)) ? OK : _.PASSWORD_IS_TOO_WEAK
 }
 
 // @Note: must be called after `password` validator, because it depends on value set by that function
 password.confirm = (value) => {
-  return (value === password.value) ? OK : 'Password mismatch'
+  return (value === password.value) ? OK : _.PASSWORD_MISMATCH
 }
 
 export function phoneNumber (value) {
-  return (!value || isPhoneNumber(value)) ? OK : 'Enter phone number with country code'
+  return (!value || isPhoneNumber(value)) ? OK : _.ENTER_NUMBER_STARTING_WITH_plus_COUNTRY_CODE
 }
 
 export function dateMonthYear (value) {
-  return /^([0-3])?[0-9]\.([0-1])?[0-9]\.\d+$/.test(value) ? OK : 'Invalid date'
+  return /^([0-3])?[0-9]\.([0-1])?[0-9]\.\d+$/.test(value) ? OK : _.INVALID_DATE
 }
 
 export function timeInThePast (value) {
   if (!isNumeric(value)) return OK
   const now = Date.now()
-  return value >= now ? 'Must be in the past' : OK
+  return value >= now ? _.MUST_BE_IN_THE_PAST : OK
 }
 
 export function timeRangesInFuture (value) {
   if (isEmpty(value)) return OK
   const now = Date.now()
-  return toList(value).find(({from, to}) => (from <= now || to <= now)) ? 'Must be in the future' : OK
+  return toList(value).find(({from, to}) => (from <= now || to <= now)) ? _.MUST_BE_IN_THE_FUTURE : OK
 }
 
 // Validate that Time Ranges contain `from` and `to` time
 export function timeRanges (value) {
   if (isEmpty(value)) return OK
   const values = toList(value)
-  if (!values.find(({from}) => from != null)) return 'Must have start time'
-  if (!values.find(({to}) => to != null)) return 'Must have end time'
+  if (!values.find(({from}) => from != null)) return _.MUST_HAVE_START_TIME
+  if (!values.find(({to}) => to != null)) return _.MUST_HAVE_END_TIME
   return OK
 }
