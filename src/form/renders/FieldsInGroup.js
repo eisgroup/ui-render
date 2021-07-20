955fa926777fc5fbce73416e161530adaa3a563a
Chris Co <coswarm@gmail.com>
2021-07-20 22:36:10 +0300

    fix FieldsInGroup validation + label

--- src/form/renders/FieldsInGroup.js
+++ src/form/renders/FieldsInGroup.js
@@ -1,12 +1,16 @@
-import classNames from 'classnames'
 import { FIELD } from 'modules-pack/variables'
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
+import { Field } from 'react-final-form'
+import { cn } from 'react-ui-pack'
 import Label from 'react-ui-pack/Label'
 import Row from 'react-ui-pack/Row'
 import Tabs from 'react-ui-pack/Tabs'
+import View from 'react-ui-pack/View'
 import { Active, get } from 'utils-pack'
 import { withGroupInputChange } from '../utils'
+
+if (!Active.Field) Active.Field = Field
 
 /**
  * Semantically Related Fields Rendered Together (as single Row by default)
@@ -28,15 +32,26 @@ export default class FieldsInGroup extends PureComponent {
   }
 
   renderContainer = () => {
-    const {kind, className, style, onChange, instance, items: __, float, initialValues, ...containerProps} = this.props
+    const {
+      kind, onChange, float, initialValues, instance, items: __,
+      hint, label, className, style, ...props
+    } = this.props
     const items = this.fields
-    const props = {className: classNames('fields-in-group top justify', className), style, ...containerProps}
     switch (kind) {
+      /**
+       * @Note: Tabs is a special use case, because inactive tabs do not register inputs.
+       *        If unregistered input is required, validation does not work.
+       *        If input has changed, but not in visible tab, `registeredValues` does not work.
+       *        => thus always register input so validation and form changes work as expected.
+       */
       case FIELD.TYPE.TABS: // turn each item label to Tab, the rest as tab content
         props.tabs = []
         props.panels = []
+        props.children = (activeIndex) => items
+          .filter((f, i) => i !== activeIndex)
+          .map(({name, validate}) => <Active.Field key={name} name={name} validate={validate} component={() => null}/>)
         items.forEach(({label, ...field}) => {
-          props.tabs.push(label || field.name)
+          props.tabs.push(<Label className={cn({required: field.required})}>{label || field.name}</Label>)
           props.panels.push(() => {
             // @Note: currently only supports uploads for a single file, for multiple files, use UploadGrids.
             //        => this is because initialValues gets reset to only changed files on tab changes.
@@ -46,7 +61,7 @@ export default class FieldsInGroup extends PureComponent {
             if (field.view === FIELD.TYPE.UPLOAD_GRID) {
               // Set initialValues to changeValues to recover changed state on tab changes
               const name = [field.name]
-              if (containerProps.name) name.unshift(containerProps.name)
+              if (props.name) name.unshift(props.name)
               field.initialValues = get(instance.formValues, name) || field.initialValues
             }
             return Active.renderField(field)
@@ -55,7 +70,7 @@ export default class FieldsInGroup extends PureComponent {
         return <Tabs {...props} />
       default:
         return (
-          <Row {...props}>
+          <Row className="top justify" {...props}>
             {items.map(Active.renderField)}
           </Row>
         )
@@ -63,13 +78,13 @@ export default class FieldsInGroup extends PureComponent {
   }
 
   render () {
-    const {hint, label} = this.props
+    const {hint, label, className, style} = this.props
     return (
-      <>
+      <View className={cn('fields-in-group', className)} style={style}>
         {hint && <Label>{hint}</Label>}
         {label && <Label>{label}</Label>}
         {this.renderContainer()}
-      </>
+      </View>
     )
   }
 }
