a476cbb8d589c2725674a88cd05c87c5c3db8e40
Chris Co <coswarm@gmail.com>
2021-07-28 14:29:52 +0300

    fix base64 css background image

--- src/utils/css.js
+++ src/utils/css.js
@@ -1,4 +1,4 @@
-import { fileExtensionNormalized, isFileSrc } from 'utils-pack'
+import { isFileSrc } from 'utils-pack'
 import { STYLE } from '../styles'
 
 /**
@@ -12,7 +12,7 @@ export function cssBgImageFrom ({src, name}) {
   // Since base64 encoded string is usually large, it's better to check if `src` is URL or Path.
   // If not, default to base64 format, because that is the only other format valid for use as css url.
   // This logic works, especially when `src` can be String object, which fails with isBase64() check.
-  return `url('${isFileSrc(src) ? encodeURI(src) : `data:image/${fileExtensionNormalized(name) || 'png'};base64, ${src}`}')`
+  return `url('${isFileSrc(src) ? encodeURI(src) : src}')`
 }
 
 /**
