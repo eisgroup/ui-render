7d5b0f73100b6f6e0d6707195d7e01ceddaeb756
Chris Co <coswarm@gmail.com>
2022-01-23 07:07:53 +0300

    refactor database to have DB_USER and DB_PASS with hostname dot notation

--- src/utils/mongoose/database.js
+++ src/utils/mongoose/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import sanitizer from 'mongoose-sanitizer'
 import { __TEST__ } from 'utils-pack'
-import { DB_HOST, DB_NAME, DB_PORT, DOMAIN } from '../server/config'
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, DOMAIN } from '../server/config'
 
 /**
  * DATABASE CONFIG =============================================================
@@ -12,17 +12,17 @@ import { DB_HOST, DB_NAME, DB_PORT, DOMAIN } from '../server/config'
 if (__TEST__) {
   console.log(`⚡  Mongoose disabled for JEST tests`)
 } else {
-  const uri = `mongodb://${DB_HOST}${DOMAIN ? ('-' + DOMAIN) : ''}:${DB_PORT}/${DB_NAME}`
+  const uri = `mongodb://${DB_HOST}${DOMAIN ? ('.' + DOMAIN) : ''}:${DB_PORT}/${DB_NAME}`
+  const options = (DB_USER && DB_PASS) ? {user: DB_USER, pass: DB_PASS} : {}
   mongoose.Promise = Promise  // use Promise, instead of default Node.js function (err, ...) callbacks
   mongoose.plugin(sanitizer)
   // mongoose.plugin(uniqueValidator) // causes Error in Mongoose 6
-  mongoose.connect(uri, {
-    // Mongoose 5.7.5 only options
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
-  })
+  // Mongoose 5.7.5 only options
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
+  mongoose.connect(uri, options)
     .then(() => console.log(`⚡  Mongoose connected to ${uri}`))
     .catch(err => {
       console.error(`✋  Mongoose ${uri} connection failed with:` + err)
