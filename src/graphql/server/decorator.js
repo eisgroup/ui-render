c9b5cf932cf294e10374bb65aefe6531c01c962e
Chris Co <coswarm@gmail.com>
2022-01-23 06:04:15 +0300

    fix graphql exportTo webpack prod bundle

--- src/graphql/server/decorator.js
+++ src/graphql/server/decorator.js
@@ -20,24 +20,42 @@ localiseTranslation({
  * GraphQL Decorator to Export Resolvers Object using Class-style Syntax
  * @example:
  *    const resolvers = {}
- *   *@exportTo(resolvers)
- *    class Query {
+ *   *@exportTo.Query(resolvers)
+ *    class Query { // note that class Query.name will not be 'Query' when bundled by webpack in backend
  *      user (parent, args, context, info) {
  *        // ...resolver logic
  *      }
  *    }
  *    export default resolvers
  *
+ * @param {String} Name - resolver to extend, one of ['Query', 'Mutation', 'Subscription']
  * @param {Object} resolvers - plain js object to be used as GQL resolvers for Query, Mutation, Subscription...
  * @returns {(function(*=): void)|*} decorator - that converts Class-style methods to plain object resolvers for GQL
  */
-export function exportTo (resolvers) {
+export function exportTo (Name, resolvers) {
   return function Decorator (Class) {
     const instance = new Class()
     const nestedResolvers = {}
     classInstanceMethodNames(Class).forEach(resolver => nestedResolvers[resolver] = instance[resolver])
-    resolvers[Class.name] = nestedResolvers
+    if (Name) {
+      resolvers[Name] = nestedResolvers
+    } else {
+      Object.assign(resolvers, nestedResolvers)
+    }
   }
+}
+
+// @see: exportTo() docs
+exportTo.Query = function (...args) {
+  return exportTo('Query', ...args)
+}
+// @see: exportTo() docs
+exportTo.Mutation = function (...args) {
+  return exportTo('Mutation', ...args)
+}
+// @see: exportTo() docs
+exportTo.Subscription = function (...args) {
+  return exportTo('Subscription', ...args)
 }
 
 /**
