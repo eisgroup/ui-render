1af94b5ecf7ceedd015adf0cf89b023163104522
Chris Co <coswarm@gmail.com>
2022-03-30 06:35:37 +0300

    fix idFromRoute logic to slug

--- src/popup/actions.js
+++ src/popup/actions.js
@@ -5,7 +5,7 @@ import { POPUP } from './constants'
 /**
  * Open Popup Alert with given messages
  * @param {String|Number|Node|JSX} title
- * @param {String|Number|Node|JSX} content
+ * @param {String|Number|Node|JSX} [content]
  */
 export function popupAlert (title, content) {
   Active.store.dispatch(stateAction(POPUP, ALERT, {items: [{title, content}]}))
