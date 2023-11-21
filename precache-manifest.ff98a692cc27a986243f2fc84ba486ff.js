470794ba38349bcee8f9b03a118fc4f0f66c7ef3
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2023-11-21 11:40:14 +0100

    Update v0.30.9

---
+++ precache-manifest.ff98a692cc27a986243f2fc84ba486ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "43e05fb4cbd9605ae962",
+    "url": "/ui-render/static/js/main.43e05fb4.chunk.js"
+  },
+  {
+    "revision": "5290ee23ee643373cf58",
+    "url": "/ui-render/static/css/1.3700524c.chunk.css"
+  },
+  {
+    "revision": "5290ee23ee643373cf58",
+    "url": "/ui-render/static/js/1.5290ee23.chunk.js"
+  },
+  {
+    "revision": "0ca886b24d31e9a0177f",
+    "url": "/ui-render/static/js/runtime~main.0ca886b2.js"
+  },
+  {
+    "revision": "5436d802cf9fb35b86b8ed9fba3d7012",
+    "url": "/ui-render/static/media/changelog.5436d802.md"
+  },
+  {
+    "revision": "804c581fe9eacbe5f67ce0a9b8377150",
+    "url": "/ui-render/static/media/config.804c581f.md"
+  },
+  {
+    "revision": "5ce88312990debe7a1ffdb169afe9e24",
+    "url": "/ui-render/static/media/docs.5ce88312.md"
+  },
+  {
+    "revision": "81834152ac052eedc335507e485116c3",
+    "url": "/ui-render/static/media/faq.81834152.md"
+  },
+  {
+    "revision": "cee61bfd1974a3888a328747fbb55e3b",
+    "url": "/ui-render/static/media/styles.cee61bfd.md"
+  },
+  {
+    "revision": "91366dc23cdf4334845dd5f06d7f4bf5",
+    "url": "/ui-render/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui-render/precache-manifest.aab8723fb187e5d4912a036323a07121.js"
+  "/ui-render/precache-manifest.ff98a692cc27a986243f2fc84ba486ff.js"
 );
 
 workbox.clientsClaim();
