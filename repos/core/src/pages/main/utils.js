66989d38050e3cce48993f33de0a305984032a73
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2024-02-02 12:19:06 +0100

    EPBDS-14245 entered Date changes automatically when time zone is changed

--- repos/core/src/pages/main/utils.js
+++ repos/core/src/pages/main/utils.js
@@ -219,9 +219,9 @@ const changeOptionOrderForSelectFields = (data, meta) => {
   Return date in format 'YYYY-MM-DD'
  */
 export const getDateStringFromDateObject = (date) => {
-  const year = String(date.getFullYear()).padStart(4, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const day = String(date.getDate()).padStart(2, '0')
+  const year = String(date.getUTCFullYear()).padStart(4, '0')
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0')
+  const day = String(date.getUTCDate()).padStart(2, '0')
   return `${year}-${month}-${day}`
 }
 
